0bbdc19ea4ebe5f91733ea5a524ca2509d1da3e3
thread <thethread@gmail.com>
2017-09-16 16:05:51 -0400

    ..

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var RoonApi = require("node-roon-api");
+var RoonApi          = require("node-roon-api"),
+    RoonApiStatus    = require("node-roon-api-status"),
+    RoonApiTransport = require("node-roon-api-transport");
 
 var roon = new RoonApi({
     extension_id:        'com.threadbox.rune-remote',
@@ -6,9 +8,38 @@ var roon = new RoonApi({
     display_version:     "1.0.0",
     publisher:           'Thread',
     email:               'thethread@gmail.com',
-    website:             'https://threadbox.net/'
+    website:             'https://threadbox.net/',
+
+    core_paired: function(core) {
+        let transport = core.services.RoonApiTransport;
+        transport.subscribe_zones(function(cmd, data) {
+            console.log("PAIRED");
+                                    console.log(core.core_id,
+                                                core.display_name,
+                                                core.display_version,
+                                                "-",
+                                                cmd,
+                                                JSON.stringify(data, null, '  '));
+                                });
+    },
+
+    core_unpaired: function(core) {
+            console.log("UNPAIRED");
+                console.log(core.core_id,
+                        core.display_name,
+                        core.display_version,
+                        "-",
+                        "LOST");
+    }
 });
 
-roon.init_services({});
+var svc_status = new RoonApiStatus(roon);
+
+roon.init_services({
+    required_services: [ RoonApiTransport ],
+    provided_services: [ svc_status ],
+});
+
+svc_status.set_status("All is good", false);
 
 roon.start_discovery();
