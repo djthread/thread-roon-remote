c723e1a65de5aedd74ccf1c9a564b6cb11643ae4
Thread <thethread@gmail.com>
2017-09-16 21:11:17 +0000

    .

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-var RoonApi          = require("node-roon-api"),
-    RoonApiStatus    = require("node-roon-api-status"),
-    RoonApiTransport = require("node-roon-api-transport");
+var RoonApi          = require('node-roon-api'),
+    RoonApiStatus    = require('node-roon-api-status'),
+    RoonApiTransport = require('node-roon-api-transport');
 
 var roon = new RoonApi({
-    extension_id:        'com.threadbox.rune-remote',
-    display_name:				 "Thread's Roon Remote",
-    display_version:     "1.0.0",
-    publisher:           'Thread',
-    email:               'thethread@gmail.com',
-    website:             'https://threadbox.net/',
+    extension_id:       'com.threadbox.rune-remote',
+    display_name:       "Thread's Roon Remote",
+    display_version:    '1.0.0',
+    publisher:          'Thread',
+    email:              'thethread@gmail.com',
+    website:            'https://threadbox.net/',
 
     core_paired: function(core) {
         let transport = core.services.RoonApiTransport;
+
         transport.subscribe_zones(function(cmd, data) {
             console.log("PAIRED");
-                                    console.log(core.core_id,
-                                                core.display_name,
-                                                core.display_version,
-                                                "-",
-                                                cmd,
-                                                JSON.stringify(data, null, '  '));
-                                });
+            console.log(core.core_id,
+        	    core.display_name,
+        	    core.display_version,
+        	    "-",
+        	    cmd,
+        	    JSON.stringify(data, null, '  '));
+        });
     },
 
     core_unpaired: function(core) {
-            console.log("UNPAIRED");
-                console.log(core.core_id,
-                        core.display_name,
-                        core.display_version,
-                        "-",
-                        "LOST");
+        console.log("UNPAIRED");
+        console.log(core.core_id,
+            core.display_name,
+            core.display_version,
+            "-",
+            "LOST");
     }
 });
 
