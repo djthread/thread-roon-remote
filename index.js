efbc5a3896458cd214ab5d9021fae1775bc8f93b
Thread <thethread@gmail.com>
2017-09-16 22:09:09 +0000

    ok..

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-let RoonApi          = require('node-roon-api'),
-    RoonApiSettings  = require('node-roon-api-settings'),
-    RoonApiStatus    = require('node-roon-api-status'),
-    RoonApiTransport = require('node-roon-api-transport');
+let RoonApi        = require('node-roon-api'),
+  RoonApiSettings  = require('node-roon-api-settings'),
+  RoonApiStatus    = require('node-roon-api-status'),
+  RoonApiTransport = require('node-roon-api-transport');
 
-let core, transport;
+let core, transport,
+  zone = '160115e8162dfe46cdcd8ea578ecefa359a3'; // JukePi Hugo2
 
 var roon = new RoonApi({
-    extension_id:       'com.threadbox.rune-remote',
-    display_name:       "Thread's Roon Remote",
-    display_version:    '1.0.0',
-    publisher:          'Thread',
-    email:              'thethread@gmail.com',
-    website:            'https://threadbox.net/',
+  extension_id:       'com.threadbox.rune-remote',
+  display_name:       "Thread's Roon Remote",
+  display_version:    '1.0.0',
+  publisher:          'Thread',
+  email:              'thethread@gmail.com',
+  website:            'https://threadbox.net/',
 
-    core_paired: function(core_) {
-        core = core_;
-        transport = core.services.RoonApiTransport;
+  core_paired: function(core_) {
+    console.log("PAIRED");
+    core = core_;
+    transport = core.services.RoonApiTransport;
 
-        transport.subscribe_zones(function(cmd, data) {
-            console.log("PAIRED");
-            console.log(core.core_id,
-        	    core.display_name,
-        	    core.display_version,
-        	    "-",
-        	    cmd,
-        	    JSON.stringify(data, null, '  '));
-        });
-    },
+    transport.subscribe_zones(function(cmd, data) {
+      console.log("ZONES SUBSCRIBED");
+      console.log(core.core_id,
+        core.display_name,
+        core.display_version,
+        "-",
+        cmd,
+        JSON.stringify(data, null, '  '));
+    });
+  },
 
-    core_unpaired: function(core_) {
-        core = undefined;
+  core_unpaired: function(core_) {
+    core = undefined;
 
-        console.log("UNPAIRED");
-        console.log(core.core_id,
-            core.display_name,
-            core.display_version,
-            "-",
-            "LOST");
-    }
+    console.log("UNPAIRED");
+    console.log(core.core_id,
+      core.display_name,
+      core.display_version,
+      "-",
+      "LOST");
+  }
 });
 
 var svc_status = new RoonApiStatus(roon);
 
 roon.init_services({
-    required_services: [ RoonApiTransport ],
-    provided_services: [ svc_status ],
+  required_services: [ RoonApiTransport ],
+  provided_services: [ svc_status ],
 });
 
-svc_status.set_status("All is good", false);
+svc_status.set_status('All is good', false);
+
+setTimeout(() => {
+  // core.services.RoonApiTransport.control(zone, 'playpause');
+  core.services.RoonApiTransport.control(zone, 'next');
+  core.services.RoonApiTransport.control(zone, 'previous');
+}, 2000);
 
 roon.start_discovery();
