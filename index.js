cd4db3fa4cf9767f27646733488f52996ac35da3
Thread <thethread@gmail.com>
2017-09-22 22:57:46 -0400

    working for me

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let joystick = new (require('joystick'))(0, 3500, 350),
   RoonApiStatus    = require('node-roon-api-status'),
   RoonApiTransport = require('node-roon-api-transport');
 
+const { exec } = require('child_process');
+
 let core,
   zone = '160115e8162dfe46cdcd8ea578ecefa359a3'; // JukePi Hugo2
 
@@ -58,24 +60,40 @@ joystick.on('button', (ev) => {
   if (!core || ev.value != 1) return;
 
   switch (ev.number) {
-    case 0:
+    case 9: // start
       console.log('Button 0 - previous');
       core.services.RoonApiTransport.control(zone, 'previous');
       break;
-    case 1:
+    case 0: // b
       console.log('Button 1 - playpause');
       core.services.RoonApiTransport.control(zone, 'playpause');
       break;
-    case 2:
+    case 1: // a
       console.log('Button 2 - next');
       core.services.RoonApiTransport.control(zone, 'next');
+      break;
+    case 8: // select
+      exec("shutdown -h now");
       break;
     default:
       console.log('UNMAPPED JOYSTICK EVENT', ev);
   }
-  // { time: 37560, value: 1, number: 3, type: 'button', id: 0 }
-  // { time: 37660, value: 1, number: 2, type: 'button', id: 0 }
-  // { time: 37730, value: 0, number: 3, type: 'button', id: 0 }
+  // { time: .., value: 1, number: 1, type: 'button', id: 0 } # a
+  // { time: .., value: 0, number: 1, type: 'button', id: 0 }
+  // { time: .., value: 1, number: 0, type: 'button', id: 0 } # b
+  // { time: .., value: 0, number: 0, type: 'button', id: 0 }
+  // { time: .., value: 1, number: 9, type: 'button', id: 0 } #start
+  // { time: .., value: 0, number: 9, type: 'button', id: 0 }
+  // { time: .., value: 1, number: 8, type: 'button', id: 0 } #select
+  // { time: .., value: 0, number: 8, type: 'button', id: 0 }
+  // { time: .., value: -32767, number: 1, type: 'axis', id: 0 } #up
+  // { time: .., value: 0, number: 1, type: 'axis', id: 0 }
+  // { time: .., value: 32767, number: 1, type: 'axis', id: 0 } #down
+  // { time: .., value: 0, number: 1, type: 'axis', id: 0 }
+  // { time: .., value: -32767, number: 0, type: 'axis', id: 0 }#left
+  // { time: .., value: 0, number: 0, type: 'axis', id: 0 }
+  // { time: .., value: 32767, number: 0, type: 'axis', id: 0 }#right
+  // { time: .., value: 0, number: 0, type: 'axis', id: 0 }
 });
 
 roon.start_discovery();
