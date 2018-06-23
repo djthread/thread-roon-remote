4837f7fbd8cf5f659637b89098f429137b509506
thread <thethread@gmail.com>
2018-06-23 16:38:37 -0400

    sparky / sweet16

--- sparky.js
+++ sparky.js
@@ -4,26 +4,28 @@ const log = require('./lib/log'),
   // mpd = require('./lib/mpd'),
   // ashuffle = require('./lib/ashuffle'),
   roon = require('./lib/roon'),
-  mySweet16 = require('./lib/keybaord.sweet16'),
+  mySweet16 = require('./lib/keyboard.sweet16'),
   keyboard = require('./lib/keyboard'),
   actions = require('./lib/actions');
 
 // JukePi Hugo2 zone
 // const zone = '160115e8162dfe46cdcd8ea578ecefa359a3';
 // JukePi Hugo2 output
-const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
+// const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
+// Sparky / USBridge Hugo2
+const output = '16013e61fba2fb9d85fdcac3b7b23776669c';
 
 let initial = true;
 // mpd.connect();
-roon.start("Thread's Pi Roon Remote", output, () => {
+roon.start("Thread's Sparky Roon Remote", output, () => {
   if (initial) {
     roon.control('play');
     // initial = false;
   }
 });
 
-joystick.start();
-joystick.subscribe((btn) => {
-  let action = myJoystick.btnToAction(btn);
-  actions.run(action, roon, null, ashuffle);
+keyboard.start();
+keyboard.subscribe((btn) => {
+  let action = mySweet16.btnToAction(btn);
+  actions.run(action, roon, null, null);
 });
