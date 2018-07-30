1888196964e3f3ddb39e14b26148d0c682c666a4
thread <thethread@gmail.com>
2018-07-29 22:18:10 -0400

    pi does rpi

--- pi.js
+++ pi.js
@@ -6,12 +6,15 @@ const log = require('./lib/log'),
   roon = require('./lib/roon'),
   myJoystick = require('./lib/my.joystick'),
   joystick = require('./lib/joystick'),
-  actions = require('./lib/actions');
+  actions = require('./lib/actions'),
+  fs = require('fs');
 
 // JukePi Hugo2 zone
 // const zone = '160115e8162dfe46cdcd8ea578ecefa359a3';
 // JukePi Hugo2 output
-const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
+// const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
+
+const output = fs.readFileSync("zone_id", "utf8").trim();
 
 let initial = true;
 mpd.connect();
