68d0f8b06f6f1c896c2e509dbfdf0d87c4e0e4ce
thread <thethread@gmail.com>
2018-08-20 17:07:02 +0100

    sparky stuff

--- sparky.js
+++ sparky.js
@@ -9,22 +9,22 @@ const fs = require('fs'),
   keyboard = require('./lib/keyboard'),
   actions = require('./lib/actions');
 
-// JukePi Hugo2 zone
-// const zone = '160115e8162dfe46cdcd8ea578ecefa359a3';
-// JukePi Hugo2 output
-// const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
-// Sparky / USBridge Hugo2
-// const output = '16013e61fba2fb9d85fdcac3b7b23776669c';
-const output = fs.readFileSync("zone_id", "utf8").trim();
-
-let initial = true;
-// mpd.connect();
-roon.start("Thread's Sparky Roon Remote", output, () => {
-  if (initial) {
-    roon.control('play');
-    // initial = false;
-  }
-});
+// // JukePi Hugo2 zone
+// // const zone = '160115e8162dfe46cdcd8ea578ecefa359a3';
+// // JukePi Hugo2 output
+// // const output = '170115e8162dfe46cdcd8ea578ecefa359a3';
+// // Sparky / USBridge Hugo2
+// // const output = '16013e61fba2fb9d85fdcac3b7b23776669c';
+// const output = fs.readFileSync("zone_id", "utf8").trim();
+//
+// let initial = true;
+// // mpd.connect();
+// roon.start("Thread's Sparky Roon Remote", output, () => {
+//   if (initial) {
+//     roon.control('play');
+//     // initial = false;
+//   }
+// });
 
 keyboard.start();
 keyboard.subscribe((btn) => {
